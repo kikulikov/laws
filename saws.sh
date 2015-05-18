8fb53068e0743247804c462d162b3e9aa19fad4b
Kirill Kulikov <kirill@top10.com>
2015-05-18 22:02:16 +0100

    sh script updated

--- saws.sh
+++ saws.sh
@@ -3,4 +3,4 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-aws ec2 describe-instances --color on --output table --query "Reservations[].Instances[].[Tags[?Key==\`type\`]|[0].Value,Tags[?Key==\`Name\`]|[0].Value,PublicIpAddress,PublicDnsName,State.Name,InstanceId]" | grep running | awk -F'|' '{print $2, $3, $4, $5, $7}'
+aws ec2 describe-instances --color on --output table --query "Reservations[].Instances[].[Tags[?Key==\`type\`]|[0].Value,Tags[?Key==\`Name\`]|[0].Value,PublicIpAddress,PublicDnsName,State.Name,InstanceId]" | grep running | awk -F'|' '{print $2, $3, $4, $5}'
