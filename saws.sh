04aea61289eeb7c6c5bf935406f3bce9039e60c2
Kirill Kulikov <kirill@top10.com>
2015-05-20 12:14:43 +0100

    sh script removed

--- saws.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o nounset
-set -o errexit
-set -o pipefail
-
-aws ec2 describe-instances --color on --output table --query "Reservations[].Instances[].[Tags[?Key==\`type\`]|[0].Value,Tags[?Key==\`Name\`]|[0].Value,PublicIpAddress,PublicDnsName,State.Name,InstanceId]" | grep running | awk -F'|' '{print $2, $3, $4, $5}'
