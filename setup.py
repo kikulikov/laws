986535c7338d77945661dc8f37bebb7a12fb0bbf
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-10-06 16:45:07 +0100

    prepared for pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'laws',
+    packages = ['laws'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Filters AWS instances',
+    author = 'Kirill Kulikov',
+    author_email = 'kirill.kulikov@gmail.com',
+    license='MIT',
+    url='https://github.com/cyrillk/list-aws',
+    download_url = 'https://github.com/cyrillk/list-aws/tarball/0.1',
+    keywords = ['aws', 'ec2'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console'
+    ],
+    install_requires=['boto', 'tabulate']
+)
