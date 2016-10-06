fa5141895a94f6afd15d04c312811e649eca42d6
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-10-06 17:42:46 +0100

    readme updated

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # laws
 
-Filters AWS instances by name or tag.
+Lists AWS EC2 instances in a short table format `laws`. Allows to filter nodes by tag or name `laws cassandra`.
 
 ## Installation
 
@@ -11,3 +11,13 @@ pip install laws
 ## PyPi link
 
 https://pypi.python.org/pypi/laws
+
+## Useful resources
+
+http://peterdowns.com/posts/first-time-with-pypi.html
+
+```
+git tag 0.X -m "Version bumped to 0.X"
+git push --tags origin master
+python setup.py sdist upload -r pypi
+```
--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup
 setup(
     name = 'laws',
     packages = ['laws'], # this must be the same as the name above
-    version = '0.4',
-    description = 'Filters AWS instances',
+    version = '0.5',
+    description = 'Lists AWS instances',
+    long_description=open('README.md', 'rt').read(),
     author = 'Kirill Kulikov',
     author_email = 'kirill.kulikov@gmail.com',
-    url='https://github.com/cyrillk/list-aws',
-    download_url = 'https://github.com/cyrillk/list-aws/tarball/0.4',
+    url='https://github.com/cyrillk/laws',
+    download_url = 'https://github.com/cyrillk/laws/tarball/0.5',
     keywords = ['aws', 'ec2'],
     entry_points = {
         'console_scripts': ['laws=laws.__main__:main'],
