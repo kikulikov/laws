5f8fc31d890df25f5b10e893e246d83886b8df6c
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-10-07 16:01:36 +0100

    Version bumped to 0.6.2

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 setup(
     name='laws',
     packages=['laws'],
-    version='0.6.1',
+    version='0.6.2',
     description='-ls- for AWS EC2 instances',
-    long_description='https://github.com/cyrillk/laws/README.md',
+    long_description='https://github.com/cyrillk/laws/blob/master/README.md',
     author='Kirill Kulikov',
     author_email='kirill.kulikov@gmail.com',
     url='https://github.com/cyrillk/laws',
-    download_url='https://github.com/cyrillk/laws/tarball/0.6.1',
+    download_url='https://github.com/cyrillk/laws/tarball/0.6.2',
     keywords=['aws', 'ec2'],
     entry_points={
         'console_scripts': ['laws=laws.__main__:main'],
